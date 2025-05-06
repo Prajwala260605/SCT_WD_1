cb90f142f8e3f258198e0299ac1f30600d28d1d8
Prajwala260605 <lingannagariprajwala@gmail.com>
2025-05-06 17:01:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-window.addEventListener('scroll', function() {
-  const navbar = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
+// Add/remove scrolled class when user scrolls
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 10) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
